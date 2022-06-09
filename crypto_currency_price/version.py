392d1929a19d55be2cb0c313e76d18abce3c8e86
Matt Krueger <mkrueger@rstms.net>
2022-06-09 00:16:17 -0500

    Bump version: 0.1.0 → 0.2.0

--- crypto_currency_price/version.py
+++ crypto_currency_price/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-06-09 00:02:05-05:00"
+__timestamp__ = "2022-06-09 00:16:17-05:00"
